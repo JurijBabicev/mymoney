4bdd1f2c5a83e09802f9fb52bcce9676a8f6afbf
Jurij Babicev <jurij.babicev@gmail.com>
2024-09-11 22:08:07 +0000

    Login form

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login to MyMoney</title>
+    <link rel="stylesheet" href="css/index.css"> 
+    <link rel="stylesheet" href="bootstrap-5.3.3/css/bootstrap.css">
+    <script src="bootstrap-5.3.3/js/bootstrap.js"></script>
+    <script src="js/jquery_3_7.js"></script>
+</head>
+<body>
+  <div class="container-sm custom_container">
+    <div class="container-sm menu_container">
+      <div class="form-floating mb-3">
+       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating mb-4">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+      <div class="d-grid gap-3">
+        <input class="btn btn-primary btn-lg fw-bold" type="submit" value="Submit">
+        <input class="btn btn-outline-secondary btn-lg fw-bold" type="submit" value="Registration">
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
